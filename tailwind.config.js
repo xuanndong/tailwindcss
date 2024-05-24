4523c4ece8009f50cc9398823f70efb56d1dccef
xuan dong <vuxuandong.004@gmail.com>
2024-05-24 16:44:29 +0700

    Add featured mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,10 +20,12 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)'},
           '100%': {transform: 'translateY(0%)'},
-        }
+        },
+        fadeIn: {from: {opacity: 0}, to: {opacity: 1} }
       },
       animation: {
-        slideDown: 'slideDown 0.3s ease-in-out'
+        slideDown: 'slideDown 0.3s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
       },
       backgroundImage: {
         'background.jpg': 'url("./image/background.jpg")'
